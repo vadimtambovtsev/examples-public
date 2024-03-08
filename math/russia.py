d8e7035c0f80bad62245a39674dc6592a0f96d28
Vadim Tambovtsev <hallovadim95@gmail.com>
2024-03-08 23:21:39 +0200

    task 96

--- math/russia.py
+++ math/russia.py
@@ -39,6 +39,7 @@ def regionals_1993_1994_class_9_task_27():
     '''
     pass
 
+
 def regionals_1994_1995_class_10_task_57():
     '''
     given: f(x) = 1/(1-x^3)^(1/3)
@@ -52,5 +53,41 @@ def regionals_1994_1995_class_10_task_57():
 
     the functions will repeat with cycle 3, therefore 95 mod 3 = 2,
     therefore f(f(...95times..(f(19)))) = f(f(19)) = (1-1/19^3)^(1/3)
+    '''
+    pass
+
+
+def regionals_1994_1995_class_10_task_61():
+    '''
+    given: f(x) = ax2+bx+c, a<b, f(x) >=0 for all x
+    find: min((a+b+c)/(b-a))
+
+    solution:
+    if f(x) >= 0 for all x, then a > 0, b^2 - 4ac <= 0, therefore c >= b2/4a
+    A = (a+b+c)/(b-a),  t = b-a > 0
+    A >= (a+b+b2/4a)/(b-a) = (4a2+4ab+b2)/4a(b-a) = (9a2+6at+t2)/4at =
+        = 3/2 + (9a2+t2)/4at >= 3/2 + sqrt(9a2t2)/2at = 3/2 + 3/2 = 3
+    '''
+    pass
+
+
+def regionals_1996_1997_class_10_task_96():
+    '''
+    given: a line with 1996 points, equal distance between points.
+        Peter colors half of them in red, the other half in blue.
+        Then Vasily divides them in pairs (red, blue).
+
+    prove: sum (red,blue) distances does not depend on the way Peter colored the points.
+
+    solution:
+        let's prove that the sum will be maximum if the pairs are created this way:
+        (leftmost red point, rightmost blue point),etc
+        - this allows for maximum overlay of the subintervals, and thus to maximize the sum
+
+        let's evaluate a section k:k+1,
+        if k = 998, it is covered with k subintervals
+        if k = 997, it is coreded with k - 1 subinvervals etc
+        ...
+        sum = 1 + 2 + ... 997 + 998 + 997 + ... 1 = 998^2 and does not depend on coloring
     '''
     pass
